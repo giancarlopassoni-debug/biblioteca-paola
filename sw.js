fa8f892c79a31f07698d53035e1696aef189bb2d
giancarlopassoni-debug <giancarlo.passoni@icloud.com>
2026-09-21 15:43:25 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='biblioteca-paola-v1.18';
+const CACHE='biblioteca-paola-v1.19';
 const ASSETS=['./','./index.html','./manifest.webmanifest','./icon-192.png','./icon-512.png'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))));
